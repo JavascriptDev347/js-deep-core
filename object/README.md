e705246be92bbf04a392bd391a21e08e5f65af45
RustambekJavascript <rustambekeshboriyev@gmail.com>
2025-03-06 10:54:11 +0500

    garbage collection

--- object/README.md
+++ object/README.md
@@ -1,3 +1,16 @@
 ## Garbage collection
 
+## It is simple example for the garbage colletion
+
 ![img.png](img.png)
+
+## Here the arrow depicts an object reference. The global variable "user" references the object {name: "John"} (we’ll call it John for brevity). The "name" property of John stores a primitive, so it’s painted inside the object.
+
+## If the value of user is overwritten, the reference is lost:
+
+![img_1.png](img_1.png)
+
+## Interlinked objects
+
+## ("./garbage_collection.js") 
+![img_2.png](img_2.png)
--- object/garbage_collection.js
+++ object/garbage_collection.js
@@ -1,2 +1,21 @@
 //What happens when something is not needed any more?
 // How does the JavaScript engine discover it and clean it up?
+
+let user = {name: "Asilbek"};
+let admin = user;
+admin.name = "Rustambek"; //user.name is changed and first value cleaned by garbage collection
+// console.log(user.name);
+
+//interlinked objects
+
+function marry(man, woman) {
+    woman.husband = man;
+    man.wife = woman;
+    return {
+        father: man,
+        mother: woman,
+    }
+};
+
+// Interlinked objects
+let family = marry({name: "Johnson"}, {name: "Asia"});
